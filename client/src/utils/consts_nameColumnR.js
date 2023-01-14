e663294fad2d5ce01f02e6632d0505992fcc8dbe
Nikita <nikki-xit@mail.ru>
2023-01-14 18:53:27 +0300

    creating selected row function

---
+++ client/src/utils/consts_nameColumnR.js
@@ -0,0 +1,52 @@
+export const CLIENT_NAMECOLUMNR = [
+  "id",
+  "ФИО",
+  "Почта",
+  "Телефон",
+  "Адрес",
+  "Скидка",
+];
+
+export const CREDENTIALS_NAMECOLUMNR = [
+  "id",
+  "ФИО",
+  "Логин",
+  "Пароль",
+  "Роль на сайте",
+];
+export const SALE_NAMECOLUMNR = [
+  "id",
+  "ФИО",
+  "Название продукта",
+  "Дата оформления",
+  "Количество Товара",
+  "Итоговая цена",
+  "Связь"
+];
+export const SALES_ARCHIVE_NAMECOLUMNR = ["id", "id документа", "Статус"];
+export const PRODUCT_NAMECOLUMNR = [
+  "id",
+  "Категория",
+  "Название",
+  "Цена",
+  "Количество на складе",
+  "Изображение товара",
+];
+export const PRODUCT_INFO_NAMECOLUMNR = ["id", "id товара", "Ключ", "Значение"];
+export const CATEGORY_NAMECOLUMNR = ["id", "Название", "Изображение категории"];
+export const POINT_ISSUE_NAMECOLUMNR = ["id", "Адрес", "Время работы"];
+export const SUPPLIERS_NAMECOLUMNR = ["id", "Название"];
+export const INVOICE_NAMECOLUMNR = [
+  "id",
+  "Поставщик",
+  "Куда доставляет",
+  "Дата оформления",
+  "Количество товара",
+  "Сумма",
+];
+export const PRODUCT_WAYBILL_NAMECOLUMNR = [
+  "id",
+  "id накладной",
+  "Название товара",
+  "Количество товара",
+];
